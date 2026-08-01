2bb8fddac122b963e6570acd5809956ee56357b3
Piyush Raj <piyushraj102000@gmail.com>
2026-08-01 20:09:46 +0530

    chore: update redis database host, port, and credentials

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -3,10 +3,11 @@ const { createClient } = require("redis");
 const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
-    socket: {
-        host: 'redis-14230.c264.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 14230
+       socket: {
+        host: 'redis-14430.crce300.ap-south-1-2.ec2.cloud.redislabs.com',
+        port: 14430
     }
 });
+
 
 module.exports=redisClient;
